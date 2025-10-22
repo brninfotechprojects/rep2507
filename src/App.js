8ee4f2e3276f3e7c5d1bdc8c23554b18c44efcca
Manjunadh <manjunadhb@gmail.com>
2025-10-22 11:50:57 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>This code is written by Manjunadh on behalf of Himaja</h1>
+      <h1>This code is written by Manjunadh</h1>
     </div>
   );
 }
